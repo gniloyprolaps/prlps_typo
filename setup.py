d3641813b94de5b7f92bedf398cde1c12c031aa1
Gniloy <nktrmnf@gmail.com>
2024-12-25 03:32:15 +0500

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='prlps_typo',
-    version='0.0.1',
+    version='0.0.2',
     author='prolapser',
     packages=find_packages(),
     url='https://github.com/gniloyprolaps/prlps_typo',
@@ -11,6 +11,9 @@ setup(
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     install_requires=['py_mini_racer', 'html2text', 'mistune', 'httpx'],
+    package_data={
+        'prlps_typo': ['typograf.js'],
+    },
     python_requires='>=3.10',
     classifiers=[
         'Development Status :: 4 - Beta',
